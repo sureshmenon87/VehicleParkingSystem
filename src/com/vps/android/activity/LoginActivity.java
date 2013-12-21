dc10e8c64b573ab5247fd17869fad8a70f57bc23
sureshmenon <sureshmenon87@bitbucket.org>
2013-12-21 11:21:04 +0530

    update

--- src/com/vps/android/activity/LoginActivity.java
+++ src/com/vps/android/activity/LoginActivity.java
@@ -24,6 +24,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity implements ServiceCallback{
 
@@ -49,6 +50,8 @@ public class LoginActivity extends Activity implements ServiceCallback{
 
 		Button btnLogin=(Button)findViewById(R.id.btnLogin);
 		userName=(EditText)findViewById(R.id.txtUserName);
+		password=(EditText)findViewById(R.id.txtPassword);
+
 		serviceModel=new ServiceModel();
 		layoutLogin=(RelativeLayout)findViewById(R.id.loginPanel);
 		layoutTriangle=(RelativeLayout)findViewById(R.id.shapeTriangle);
@@ -73,21 +76,27 @@ public class LoginActivity extends Activity implements ServiceCallback{
 			@Override
 			public void onClick(View arg0) {
 
-				PropertyInfo propertyInfo=new PropertyInfo();
-				propertyInfo.setName("UserID");
-				propertyInfo.setValue("admin");
-				propertyInfo.setType(PropertyInfo.STRING_CLASS);
-				serviceModel.addProperty(propertyInfo);
 
-				propertyInfo=new PropertyInfo();
-				propertyInfo.setName("Password");
-				propertyInfo.setValue("admin");
-				propertyInfo.setType(PropertyInfo.STRING_CLASS);
-				serviceModel.addProperty(propertyInfo);
-				serviceModel.setServiceOperation("IsValidUser");
-				WebServiceEngine engine=new WebServiceEngine(LoginActivity.this);
-				engine.setServiceModel(serviceModel);
-				engine.start();
+				Log.i("VPS","UN:"+userName.getEditableText().toString());
+				if(userName.getEditableText().toString().length()!=0 && password.getEditableText().toString().length()!=0){
+					PropertyInfo propertyInfo=new PropertyInfo();
+					propertyInfo.setName("UserID");
+					propertyInfo.setValue(userName.getEditableText().toString());
+					propertyInfo.setType(PropertyInfo.STRING_CLASS);
+					serviceModel.addProperty(propertyInfo);
+
+					propertyInfo=new PropertyInfo();
+					propertyInfo.setName("Password");
+					propertyInfo.setValue(password.getEditableText().toString());
+					propertyInfo.setType(PropertyInfo.STRING_CLASS);
+					serviceModel.addProperty(propertyInfo);
+					serviceModel.setServiceOperation("IsValidUser");
+					WebServiceEngine engine=new WebServiceEngine(LoginActivity.this);
+					engine.setServiceModel(serviceModel);
+					engine.start();
+				}else{
+					Toast.makeText(LoginActivity.this,"Username/Password cannot be left empty", 3000).show();
+				}
 
 			}
 		});
@@ -98,29 +107,39 @@ public class LoginActivity extends Activity implements ServiceCallback{
 	@Override
 	public void onResult(ServiceModel model) {
 		Log.i("VPS","onResult :"+model.getServiceOperation());
+		String result="-1";
 		if (model.getServiceOperation().equals("IsValidDevice")) {
 			sharedPrefs.edit().putString("DeviceCode", serviceModel.getResult().toString());
 			runOnUiThread(new Runnable() {
 
 				@Override
 				public void run() {
-					
+
 					layout01.setVisibility(RelativeLayout.VISIBLE);
 					layoutLogin.setVisibility(RelativeLayout.VISIBLE);
 					layoutTriangle.setVisibility(RelativeLayout.VISIBLE);
 					deviceCheck.setVisibility(RelativeLayout.INVISIBLE);
-					
+
 
 				}
 			});
 
 		}else if(model.getServiceOperation().equals("IsValidUser")){
 
-		sharedPrefs.edit().putString("UserCode",serviceModel.getResult().toString());
-		
-		Intent intent=new Intent(LoginActivity.this,EntryExitActivity.class);
-		(LoginActivity.this).startActivity(intent);
-		
+			result=serviceModel.getResult().toString();
+			if(result.equals("0")){
+				runOnUiThread("Invalid username or password.");
+			}else if(!result.equals("0") && !result.equals("-1") ){
+
+				sharedPrefs.edit().putString("UserCode",serviceModel.getResult().toString());
+
+				Intent intent=new Intent(LoginActivity.this,EntryExitActivity.class);
+				(LoginActivity.this).startActivity(intent);
+			}else if(result.equals("-1")){
+				runOnUiThread("Please try again later.");
+			}
+
+
 		}
 
 	}
@@ -130,6 +149,17 @@ public class LoginActivity extends Activity implements ServiceCallback{
 
 		TelephonyManager mngr = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE); 
 		return mngr.getDeviceId();
+	}
+
+	private void runOnUiThread(final String msg){
+		runOnUiThread(new Runnable() {
+
+			@Override
+			public void run() {
+
+				Toast.makeText(LoginActivity.this,msg,Toast.LENGTH_LONG).show();	
+			}
+		});			
 	}
 
 
